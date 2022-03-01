314beb9f184f903477ef8ae270c23c1a80b49617
Luis Vegas <luisvegasmor@gmail.com>
2022-03-01 17:38:37 -0400

    Sidebar placeholder, just in case

---
+++ src/components/SideBar_.js
@@ -0,0 +1,27 @@
+import "../App.scss";
+import Logo from "../img/white_logo.png";
+
+const SideBar = () => {
+  return (
+    <div class="sidebar">
+      <img class="logo" src={Logo} />
+      <a id="about" href="">
+        About us
+      </a>
+      <a id="partners" href="#2">
+        Partners
+      </a>
+      <a id="portfolio" href="/">
+        Portfolio
+      </a>
+      <a id="news" href="">
+        News
+      </a>
+      <a id="contact" href="">
+        Contact
+      </a>
+    </div>
+  );
+};
+
+export default SideBar;
