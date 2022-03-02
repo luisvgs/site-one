3468fe9820c2f7c20363db16cc4710e965138090
bluretrece <luisvegasmor@gmail.com>
2022-03-02 00:45:56 -0400

    Comments

--- src/state.jsx
+++ src/state.jsx
@@ -1,4 +1,10 @@
 import { proxy } from "valtio";
+
+/* Menu state behaves as follows: 
+    First scene represents index 1
+    Second scene represents index 2
+    And so on..*/
+
 export const state = proxy({
   clicked: 0,
 });
