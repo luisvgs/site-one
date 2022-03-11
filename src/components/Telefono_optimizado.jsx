08fe671e1bf5e89be6ea0e116ebfe2168b453658
Luis Vegas <luisvegasmor@gmail.com>
2022-03-11 14:15:40 -0600

    Fix: Centered html

--- src/components/Telefono_optimizado.jsx
+++ src/components/Telefono_optimizado.jsx
@@ -3,9 +3,30 @@ import { useGLTF, Html } from "@react-three/drei";
 import TelefonoOpt from "../models/telefono_optimizado.glb";
 import Carrusel from "./Carousel";
 
+const Content = ({ hidden, set }) => {
+  return (
+    <Html
+      className="cont"
+      rotation={[-Math.PI, 0, 0]}
+      position={[-2.4, 0.367, 0.035]}
+      transform
+      occlude
+      onOcclude={set}
+      style={{
+        transition: "all 0.8s",
+        opacity: hidden ? 0 : 0.8,
+        transform: `scale(${hidden ? 0.0 : 0.54})`,
+      }}
+    >
+      <div className="wrapper">
+        <Carrusel />
+      </div>
+    </Html>
+  );
+};
 const Iphone = ({ ...props }) => {
   const group = useRef();
-  const { nodes, materials } = useGLTF(TelefonoOpt);
+  const { nodes, material } = useGLTF(TelefonoOpt);
   const [hidden, set] = useState();
   return (
     <group
@@ -15,15 +36,17 @@ const Iphone = ({ ...props }) => {
       scale={[1, 1, 1]}
     >
       <mesh
-        // geometry={nodes.Glass_top.geometry}
-        // material={nodes.Glass_top.material}
-        position={[0.01, 0.82, 0.03]}
+        geometry={nodes.Glass_top.geometry}
+        material={nodes.Glass_top.material}
+        position={[0.013, 0.367, 0.035]}
         rotation={[-Math.PI, 0, 0]}
         scale={0.05}
-      ></mesh>
+      >
+        <Content hiddent={hidden} set={set} />
+      </mesh>
       <mesh
-        // geometry={nodes.Screen.geometry}
-        // material={nodes.Screen.material}
+        geometry={nodes.Screen.geometry}
+        material={nodes.Screen.material}
         position={[0.01, 0.37, 0.04]}
         rotation={[-Math.PI, 0, 0]}
         scale={[0.17, 0.36, 0.05]}
@@ -35,31 +58,11 @@ const Iphone = ({ ...props }) => {
           position={[0.0126, 0.36682, 0.03467]}
           rotation={[-Math.PI, 0, 0]}
           scale={0.03}
-        >
-          <Html
-            className="cont"
-            rotation={[-Math.PI, 0, 0]}
-          // position={[0.0126, 0.36682, 0.03467]}
-            position={[0.53, 0.37, 0.03]}
-            transform
-            ref={group}
-            occlude
-            onOcclude={set}
-            style={{
-              transition: "all 0.8s",
-              opacity: hidden ? 0 : 1,
-              transform: `scale(${hidden ? 0.0 : 0.9})`,
-            }}
-          >
-            <div className="wrapper">
-              <Carrusel />
-            </div>
-          </Html>
-        </mesh>
+        ></mesh>
       </group>
       <mesh
-        // geometry={nodes.Titanium_unibody001.geometry}
-        // material={nodes.Titanium_unibody001.material}
+        geometry={nodes.Titanium_unibody001.geometry}
+        material={nodes.Titanium_unibody001.material}
         position={[0.01, 0.37, 0.03]}
         rotation={[-Math.PI, 0, 0]}
         scale={0.03}
