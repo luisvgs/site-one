28fc2238f07a5f5dbeac14d9aafab7e2b4f1f9db
Luis Vegas <luisvegasmor@gmail.com>
2022-03-22 14:25:39 -0600

    chore: enable debug mode

--- src/components/Ga.jsx
+++ src/components/Ga.jsx
@@ -2,7 +2,7 @@ import ReactGA from "react-ga";
 
 const Ga = () => {
     const GA_ID = 'G-DLY9FRMTGB';
-    ReactGA.initialize(GA_ID);
+    ReactGA.initialize(GA_ID, {debug: true});
     ReactGA.pageview(window.location.pathname + window.location.search);  
 }
 
