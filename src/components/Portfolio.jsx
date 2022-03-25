14d371487c4c393ba02bae9628c2d3c5c4d4adf4
Luis Vegas <luisvegasmor@gmail.com>
2022-03-25 08:21:02 -0600

    feat: animated text

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import IphoneLights from "./IphoneLights";
 import { Text } from "@react-three/drei";
 import Iphone from "./Telefono_optimizado";
 import IphoneBase from "./Telefono_base";
 import Content from "./Content";
+import { useSpring, animated, config } from "react-spring";
 
 const Portfolio = () => {
+  const [hovered, setHovered] = useState(false);
+  const AnimatedText = animated(Text);
+  const { wobble } = useSpring({
+    wobble: hovered ? 1 : 1.1,
+    config: config.wobbly,
+  });
+
   return (
     <>
-      <Text
+      <AnimatedText
+        onPointerOver={() => setHovered(true)}
+        onPointerOut={() => setHovered(false)}
         position={[10.6, 1.41, 2]}
-        scale={[1, 1, 1]}
+        scale={wobble}
         fontSize={0.06}
         color="white"
         maxWidth={0.69}
@@ -17,8 +28,8 @@ const Portfolio = () => {
         anchorY="middle"
       >
         Portfolio
-      </Text>
-      {/* <Iphone position={[10.27, 0.90, 2]} content={<Content />} /> */}
+      </AnimatedText>
+      <Iphone position={[10.27, 0.9, 2]} content={<Content />} />
       <IphoneLights position={[10.27, 0.88, 2]} />
       <IphoneBase position={[10.27, 0.88, 2]} />
     </>
