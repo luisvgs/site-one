5a377fcd7d8666bd22bc03d2dcccc38046d72450
Luis Vegas <luisvegasmor@gmail.com>
2022-03-30 10:52:14 -0400

    feat: app descriptio and store logos

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,11 +1,30 @@
 import { useState } from "react";
 import IphoneLights from "./IphoneLights";
-import { Text } from "@react-three/drei";
+import { Text, Image } from "@react-three/drei";
 import Iphone from "./Telefono_optimizado";
 import IphoneBase from "./Telefono_base";
 import Content from "./Content";
 import { useSpring, animated, config } from "react-spring";
 import Selector from "./Selector";
+import Playstore from "./google-arrow.png";
+import AppStore from "./appstore.jpg";
+
+const StoreLogos = () => {
+  return (
+    <>
+      <Image
+        url={Playstore}
+        position={[10.7, 1.2, 2]}
+        scale={[0.07, 0.07, 0.07]}
+      />
+      <Image
+        url={AppStore}
+        position={[10.6, 1.2, 2]}
+        scale={[0.07, 0.07, 0.07]}
+      />
+    </>
+  );
+};
 
 const Portfolio = () => {
   const [hovered, setHovered] = useState(false);
@@ -17,10 +36,11 @@ const Portfolio = () => {
 
   return (
     <>
+      <StoreLogos />
       <AnimatedText
         onPointerOver={() => setHovered(true)}
         onPointerOut={() => setHovered(false)}
-        position={[10.6, 1.41, 2]}
+        position={[10.7, 1.5, 2]}
         scale={wobble}
         fontSize={0.06}
         color="white"
@@ -30,6 +50,20 @@ const Portfolio = () => {
       >
         Portfolio
       </AnimatedText>
+      <Text
+        onPointerOver={() => setHovered(true)}
+        onPointerOut={() => setHovered(false)}
+        position={[10.67, 1.37, 2]}
+        scale={0.3}
+        fontSize={0.06}
+        color="white"
+        maxWidth={0.69}
+        anchorX="center"
+        anchorY="middle"
+      >
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea nesciunt
+        placeat recusandae id aperiam voluptatibus minus! Eos temporibus
+      </Text>
       <Iphone position={[10.27, 0.9, 2]} />
       <IphoneLights position={[10.27, 0.88, 2]} />
       <IphoneBase position={[10.27, 0.88, 2]} />
