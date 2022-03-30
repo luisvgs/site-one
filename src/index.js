8ba18924a0e829fc87eec55734e8561f405f5c26
Luis Vegas <luisvegasmor@gmail.com>
2022-03-30 09:52:28 -0400

    chore: deactivate firebase

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDlkfeL4Z2W75skK39azye8CGRpVqcusBA",
-  authDomain: "techmindsacademy-5f7f4.firebaseapp.com",
-  projectId: "techmindsacademy-5f7f4",
-  storageBucket: "techmindsacademy-5f7f4.appspot.com",
-  messagingSenderId: "29989098666",
-  appId: "1:29989098666:web:4cbed2406f2a3d7c2e2cd3",
-  measurementId: "G-ZCTKJBX4B4",
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDlkfeL4Z2W75skK39azye8CGRpVqcusBA",
+//   authDomain: "techmindsacademy-5f7f4.firebaseapp.com",
+//   projectId: "techmindsacademy-5f7f4",
+//   storageBucket: "techmindsacademy-5f7f4.appspot.com",
+//   messagingSenderId: "29989098666",
+//   appId: "1:29989098666:web:4cbed2406f2a3d7c2e2cd3",
+//   measurementId: "G-ZCTKJBX4B4",
+// };
 
-const app = initializeApp(firebaseConfig);
-getAnalytics(app);
+// const app = initializeApp(firebaseConfig);
+// getAnalytics(app);
 
 ReactDOM.render(
   <React.StrictMode>
