7a175e43be9b2211f0fc34edab2b8d903f418b70
Matthew Willox <matthew@cybermancers.ca>
2022-04-08 14:23:36 -0600

    modified the scroll effect to use the lookat and position curve

--- src/components/Setup.jsx
+++ src/components/Setup.jsx
@@ -15,25 +15,47 @@ import { useSnapshot } from "valtio";
 import { useThree, useFrame } from "@react-three/fiber";
 import { state } from "../state";
 
+
+const LOOKAT = {
+  HOME: new Vector3(-1.1, 0.1, -12.8),
+  ABOUT: new Vector3(2.2, -0.4, -12.8),
+  PARTNERS: new Vector3(7, -2.9, -12.8),
+  PORTFOLIO: new Vector3(9, -0, -12.8),
+  NEWS: new Vector3(11.7, 0, -12.8),
+  CONTACT: new Vector3(16.03, 0, 0),
+}
+
+const POSITION = {
+  HOME: new Vector3(.5, 0.1, 5.1),
+  ABOUT: new Vector3(5.1, 0.1, 4.6),
+  PARTNERS: new Vector3(10.0, 0.58, 4.3),
+  PORTFOLIO: new Vector3(16.4, 0.5, 3.9),
+  NEWS: new Vector3(20.03, 0.42, 3.8),
+  CONTACT: new Vector3(25.5, 0.5, 3.3),
+}
+
+const SCROLL_POSITION = {
+  HOME: 0,
+  ABOUT: .2,
+  PARTNERS: .4,
+  PORTFOLIO: .6,
+  NEWS: .8,
+  CONTACT: 1,
+}
+
+const SECTIONS = ["HOME", "ABOUT", "PARTNERS", "PORTFOLIO", "NEWS", "CONTACT"]
+
 // Camera path
-const cameraPositionCurve = new THREE.CatmullRomCurve3([
-  // 1ra escena
-  new Vector3(0.111, 0.11, 5.0),
-  // 2da escena
-  new Vector3(-2, 0.35, 5.0), // Partners point
-  new Vector3(-2, 0.12, 4.8), // Phone point
-  // 3ra escena
-  new Vector3(0.32, 0.8, 4.9),
-  new Vector3(0.32, 1.5, 4.9),
-]);
+const cameraPositionCurve = new THREE.CatmullRomCurve3(SECTIONS.map(k => POSITION[k]));
 
-const cameraLookAtCurve = new THREE.CatmullRomCurve3([
-  new Vector3(0, 0.2, 4),
-  new Vector3(-10, 0, -90),
-  new Vector3(-10, 0, 4),
-]);
+const cameraLookAtCurve = new THREE.CatmullRomCurve3(SECTIONS.map(k => LOOKAT[k]));
 
-const cameraLookAt = new Vector3(0, 0, 0);
+
+
+
+const lookAt = new Vector3();
+const position = new Vector3();
+
 
 const Setup = () => {
   const ref = useRef();
@@ -41,46 +63,20 @@ const Setup = () => {
 
   const scroll = useScroll();
   const { size } = useThree();
-  const page = 3;
+  const page = state.clicked;
 
   useEffect(() => {
-    // scroll.el.scrollLeft = size.width * page;
+    scroll.el.scrollLeft = (size.width*2) * SCROLL_POSITION[SECTIONS[page]];
+    console.log(scroll.el.scrollLeft)
   }, [page, scroll.el, size.width]);
 
   useFrame((state) => {
+    console.log(scroll.offset);
     const step = 0.02;
-    switch (snap.clicked) {
-      case 1:
-        state.camera.lookAt(new Vector3(2.2, -0.4, -12.8));
-        state.camera.position.lerp(new Vector3(5.1, 0.1, 4.6), step);
-        state.camera.updateProjectionMatrix();
-        break;
-      case 2:
-        state.camera.lookAt(new Vector3(7, -2.9, -12.8));
-        state.camera.position.lerp(new Vector3(10.0, 0.58, 4.3), step);
-        state.camera.updateProjectionMatrix();
-        break;
-      case 3:
-        state.camera.lookAt(new Vector3(9, -0, -12.8));
-        state.camera.position.lerp(new Vector3(16.4, 0.5, 3.9), step);
-        state.camera.updateProjectionMatrix();
-        break;
-      case 4:
-        state.camera.lookAt(new Vector3(11.7, 0, -12.8));
-        state.camera.position.lerp(new Vector3(20.03, 0.42, 3.8), step);
-        state.camera.updateProjectionMatrix();
-        break;
-      case 5:
-        state.camera.lookAt(new Vector3(16.03, 0, 0));
-        state.camera.position.lerp(new Vector3(25.5, 0.5, 3.3), step);
-        state.camera.updateProjectionMatrix();
-        break;
-      default:
-        cameraPositionCurve.getPoint(scroll.offset, state.camera.position);
-        cameraLookAtCurve.getPoint(scroll.offset, cameraLookAt);
-        state.camera.lookAt(cameraLookAt);
-        break;
-    }
+    cameraPositionCurve.getPoint(scroll.offset, position);
+    cameraLookAtCurve.getPoint(scroll.offset, lookAt);
+    state.camera.lookAt(lookAt);
+    state.camera.position.lerp(position, step);
   });
 
   return (
