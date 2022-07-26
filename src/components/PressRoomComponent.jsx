4f20569a85b2fb679b4a235a2281970e3d0a38da
Luis Vegas <luisvegasmor@gmail.com>
2022-07-26 10:03:20 -0500

    Article component

--- src/components/PressRoomComponent.jsx
+++ src/components/PressRoomComponent.jsx
@@ -2,32 +2,134 @@ import React, { useEffect, useState, useRef } from "react";
 import { useGLTF, Text } from "@react-three/drei";
 import PressModel from "../models/PressRoom.glb";
 
+const Article = ({ nodes, post }) => {
+  const { materials } = useGLTF(PressModel);
+  return (
+    <>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Article_1.geometry}
+        material={materials["Material.017"]}
+        position={[-5.11, 0.5, 1.13]}
+        rotation={[0, -Math.PI / 3, -Math.PI / 2]}
+        scale={[1.53, 0.77, 1]}
+      />
+      <Text
+        position={[-5.11, 0.5, 1.3]}
+        rotation={[0, 0.5, 0]}
+        color="black"
+        anchorX="center"
+        anchorY="middle"
+      >
+        {post.map((single_post, i) =>
+          single_post.content
+            .replace("<p>", "")
+            .replace("</p>", "")
+            .substring(0, 38)
+        )}
+      </Text>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Article_1.geometry}
+        material={materials["Material.017"]}
+        position={[-2.53, 0.51, 0.05]}
+        rotation={[0, -1.31, -Math.PI / 2]}
+        scale={[1.53, 0.77, 1]}
+      />
+      <Text
+        position={[-2.53, 0.51, 0.3]}
+        rotation={[0, 0.3, 0]}
+        color="black"
+        anchorX="center"
+        anchorY="middle"
+      >
+        {post.map((single_post, i) =>
+          single_post.content
+            .replace("<p>", "")
+            .replace("</p>", "")
+            .substring(0, 38)
+        )}
+      </Text>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Article_1.geometry}
+        material={materials["Material.017"]}
+        position={[0.09, 0.51, -0.32]}
+        rotation={[Math.PI / 2, -Math.PI / 2, 0]}
+        scale={[1.53, 0.77, 1]}
+      />
+      <Text
+        position={[0.09, 0.51, -0.21]}
+        rotation={[0, 0.0, 0]}
+        color="black"
+        anchorX="center"
+        anchorY="middle"
+      >
+        {post.map((single_post, i) =>
+          single_post.content
+            .replace("<p>", "")
+            .replace("</p>", "")
+            .substring(0, 38)
+        )}
+      </Text>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Article_1.geometry}
+        material={materials["Material.017"]}
+        position={[2.94, 0.51, 0.08]}
+        rotation={[Math.PI, -1.31, Math.PI / 2]}
+        scale={[1.53, 0.77, 1]}
+      />
+      <Text
+        position={[2.84, 0.51, 0.3]}
+        rotation={[0, -0.3, 0]}
+        color="black"
+        anchorX="center"
+        anchorY="middle"
+      >
+        {post.map((single_post, i) =>
+          single_post.content
+            .replace("<p>", "")
+            .replace("</p>", "")
+            .substring(0, 38)
+        )}
+      </Text>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Article_1.geometry}
+        material={materials["Material.017"]}
+        position={[5.39, 0.51, 1.12]}
+        rotation={[Math.PI, -Math.PI / 3, Math.PI / 2]}
+        scale={[1.53, 0.77, 1]}
+      />
+      <Text
+        position={[5.2, 0.51, 1.4]}
+        rotation={[0, -0.7, 0]}
+        color="black"
+        anchorX="center"
+        anchorY="middle"
+      >
+        {post.map((single_post, i) =>
+          single_post.content
+            .replace("<p>", "")
+            .replace("</p>", "")
+            .substring(0, 38)
+        )}
+      </Text>
+    </>
+  );
+};
+
 const PressRoomComponent = ({ ...props }) => {
   const group = useRef();
   const { nodes, materials } = useGLTF(PressModel);
   const [hovered, setHovered] = useState(false);
   const [post, setPost] = useState([]);
-
-  const Entry = ({ title }) => {
-    // post.map((single_post, index) => {
-    //   console.log(single_post.title);
-    // });
-
-    return (
-      <Text color="black" anchorX="center" anchorY="middle">
-        {title}
-      </Text>
-    );
-  };
-
-  useEffect(() => {
-    loadData();
-  }, []);
-
-  useEffect(
-    () => void (document.body.style.cursor = hovered ? "pointer" : "auto"),
-    [hovered]
-  );
 
   const loadData = async () => {
     await fetch(
@@ -41,124 +143,31 @@ const PressRoomComponent = ({ ...props }) => {
       });
   };
 
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  useEffect(
+    () => void (document.body.style.cursor = hovered ? "pointer" : "auto"),
+    [hovered]
+  );
+
+  const Entry = ({ title }) => {
+    // post.map((single_post, index) => {
+    //   console.log(single_post.title);
+    // });
+
+    return (
+      <Text color="black" anchorX="center" anchorY="middle">
+        {title}
+      </Text>
+    );
+  };
+
   return (
     <>
       <group ref={group} scale={[0.2, 0.2, 0.2]} {...props} dispose={null}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.Article_1.geometry}
-          material={materials["Material.017"]}
-          position={[-5.11, 0.5, 1.13]}
-          rotation={[0, -Math.PI / 3, -Math.PI / 2]}
-          scale={[1.53, 0.77, 1]}
-        />
-        <Text
-          position={[-5.11, 0.5, 1.3]}
-          rotation={[0, 0.5, 0]}
-          color="black"
-          anchorX="center"
-          anchorY="middle"
-        >
-          {post.map((single_post, i) =>
-            single_post.content
-              .replace("<p>", "")
-              .replace("</p>", "")
-              .substring(0, 38)
-          )}
-        </Text>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.article_2.geometry}
-          material={materials["Material.017"]}
-          position={[-2.53, 0.51, 0.05]}
-          rotation={[0, -1.31, -Math.PI / 2]}
-          scale={[1.53, 0.77, 1]}
-        />
-        <Text
-          position={[-2.53, 0.51, 0.3]}
-          rotation={[0, 0.3, 0]}
-          color="black"
-          anchorX="center"
-          anchorY="middle"
-        >
-          {post.map((single_post, i) =>
-            single_post.content
-              .replace("<p>", "")
-              .replace("</p>", "")
-              .substring(0, 38)
-          )}
-        </Text>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.article_3.geometry}
-          material={materials["Material.017"]}
-          position={[0.09, 0.51, -0.32]}
-          rotation={[Math.PI / 2, -Math.PI / 2, 0]}
-          scale={[1.53, 0.77, 1]}
-        />
-        <Text
-          position={[0.09, 0.51, -0.21]}
-          rotation={[0, 0.0, 0]}
-          color="black"
-          anchorX="center"
-          anchorY="middle"
-        >
-          {post.map((single_post, i) =>
-            single_post.content
-              .replace("<p>", "")
-              .replace("</p>", "")
-              .substring(0, 38)
-          )}
-        </Text>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.article_4.geometry}
-          material={materials["Material.017"]}
-          position={[2.94, 0.51, 0.08]}
-          rotation={[Math.PI, -1.31, Math.PI / 2]}
-          scale={[1.53, 0.77, 1]}
-        />
-        <Text
-          position={[2.84, 0.51, 0.3]}
-          rotation={[0, -0.3, 0]}
-          color="black"
-          anchorX="center"
-          anchorY="middle"
-        >
-          {post.map((single_post, i) =>
-            single_post.content
-              .replace("<p>", "")
-              .replace("</p>", "")
-              .substring(0, 38)
-          )}
-        </Text>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes.article_5.geometry}
-          material={materials["Material.017"]}
-          position={[5.39, 0.51, 1.12]}
-          rotation={[Math.PI, -Math.PI / 3, Math.PI / 2]}
-          scale={[1.53, 0.77, 1]}
-        />
-        <Text
-          position={[5.2, 0.51, 1.4]}
-          rotation={[0, -0.7, 0]}
-          color="black"
-          anchorX="center"
-          anchorY="middle"
-        >
-          {post.map((single_post, i) =>
-            single_post.content
-              .replace("<p>", "")
-              .replace("</p>", "")
-              .substring(0, 38)
-          )}
-        </Text>
+        <Article nodes={nodes} post={post} />
         <mesh
           onPointerOver={() => setHovered(true)}
           onPointerOut={() => setHovered(false)}
@@ -169,13 +178,13 @@ const PressRoomComponent = ({ ...props }) => {
           position={[-1.66, -2.59, 1.71]}
           rotation={[Math.PI / 2, -Math.PI / 2, 0]}
           scale={0.81}
+          onClick={() => {
+            console.log("Left button");
+          }}
         />
         <mesh
           onPointerOver={() => setHovered(true)}
           onPointerOut={() => setHovered(false)}
-          onClick={() => {
-            console.log("Hola");
-          }}
           castShadow
           receiveShadow
           geometry={nodes.Move_right_button001.geometry}
@@ -183,6 +192,9 @@ const PressRoomComponent = ({ ...props }) => {
           position={[1.86, -2.58, 2.1]}
           rotation={[-Math.PI / 2, Math.PI / 2, 0]}
           scale={0.81}
+          onClick={() => {
+            console.log("Right button");
+          }}
         />
         <mesh
           castShadow
