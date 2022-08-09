fdfc02656f3cad046832398961e193a18770d3e9
Luis Vegas <luisvegasmor@gmail.com>
2022-08-09 12:58:27 -0500

    Limpieza de codigo

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,10 @@
 import IphoneLights from "./IphoneLights";
 import UnoLights from "./UnoLights";
-import { Text } from "@react-three/drei";
 import ContactRoom from "./ContactScene";
-import { config, useSpring } from "@react-spring/three";
 import { useState } from "react";
 
 const Contact = () => {
   const [hovered, setHovered] = useState(false);
-  const { wobble } = useSpring({
-    wobble: hovered ? 1 : 1.1,
-    config: config.wobbly,
-  });
   return (
     <>
       <UnoLights position={[16.1, 0.88, 4.1]} />
