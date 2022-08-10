26d6804eb5c6267e6b35a31bebea9e9f8ee340d4
Luis Vegas <luisvegasmor@gmail.com>
2022-08-10 14:06:14 -0500

    Tweaks para mejorar el performance

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { useRef, useState } from "react";
-import { Canvas } from "@react-three/fiber";
-import { Loader } from "@react-three/drei";
+import React, { useLayoutEffect, useRef, useState, Suspense } from "react";
+import { Canvas, useThree } from "@react-three/fiber";
+import { AdaptiveDpr, BakeShadows, Loader, Preload } from "@react-three/drei";
 import "./App.scss";
 import Burger from "./components/Burger";
 import Menu from "./components/Menu";
@@ -10,6 +10,13 @@ import { state } from "./state";
 import Media from "react-media";
 import PhoneView from "./PhoneView";
 import DesktopView from "./DesktopView";
+import PostProcessing from "./PostProcessing";
+
+function Precompile() {
+  const { gl, scene, camera } = useThree();
+  useLayoutEffect(() => void gl.compile(scene, camera), []);
+  return null;
+}
 
 const App = () => {
   const snap = useSnapshot(state);
@@ -25,17 +32,31 @@ const App = () => {
         <Menu open={open} props={snap} setOpen={setOpen} />
       </div>
       <Canvas
+        concurrent
         dpr={[1, 2]}
-        shadows
-        colorManagement
+        performance={{ min: 0.5 }}
+        // shadows
         flat
         gl={{
+          antialias: false,
           physicallyCorrectLights: true,
         }}
       >
-        <Media queries={{ small: { maxWidth: 617 } }}>
-          {(matches) => (matches.small ? <PhoneView /> : <DesktopView />)}
-        </Media>
+        <Suspense fallback={null}>
+          <Media queries={{ small: { maxWidth: 617 } }}>
+            {(matches) =>
+              matches.small ? (
+                <PhoneView />
+              ) : (
+                <>
+                  <DesktopView /> <Preload all /> <BakeShadows />{" "}
+                  <AdaptiveDpr pixelated />
+                </>
+              )
+            }
+          </Media>
+        </Suspense>
+        <PostProcessing />
       </Canvas>
       <Loader />
     </>
