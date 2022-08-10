bdf59f45f3706db20f6031db3edbe884d6b43e58
Luis Vegas <luisvegasmor@gmail.com>
2022-08-10 14:10:38 -0500

    Desactivados controles de post processing

--- src/PostProcessing.js
+++ src/PostProcessing.js
@@ -8,25 +8,25 @@ const PostProcessing = () => {
     mid: 480,
     high: 720,
   };
-  const { width, height, intensity, luminanceSmoothing, luminanceThreshold } =
-    useControls({
-      intensity: { value: 0.2, min: 0.0, max: 1.0, step: 0.1 },
-      luminanceSmoothing: { value: 0.01, min: 0.0, max: 1.0, step: 0.01 },
-      luminanceThreshold: { value: 0.84, min: 0.1, max: 1.0, step: 0.01 },
-      width: { options: resolution },
-      height: { options: resolution },
-    });
+  // const { width, height, intensity, luminanceSmoothing, luminanceThreshold } =
+  //   useControls({
+  //     intensity: { value: 0.2, min: 0.0, max: 1.0, step: 0.1 },
+  //     luminanceSmoothing: { value: 0.01, min: 0.0, max: 1.0, step: 0.01 },
+  //     luminanceThreshold: { value: 0.84, min: 0.1, max: 1.0, step: 0.01 },
+  //     width: { options: resolution },
+  //     height: { options: resolution },
+  //   });
   return (
     <EffectComposer>
       <Noise opacity={0.02} />
       <Bloom
         blurPass={BlurPass}
-        intensity={intensity}
+        intensity={0.2}
         width={720}
         height={720}
         kernelSize={KernelSize.LARGE}
-        luminanceThreshold={luminanceThreshold}
-        luminanceSmoothing={luminanceSmoothing}
+        luminanceThreshold={0.84}
+        luminanceSmoothing={0.01}
       />
     </EffectComposer>
   );
