7de812829892f5b392e27389241ab2aea36b7a90
Luis Vegas <luisvegasmor@gmail.com>
2022-08-15 10:50:30 -0400

    Activado solo PressRoom para propositos de debug

--- src/components/Setup/Setup.jsx
+++ src/components/Setup/Setup.jsx
@@ -74,16 +74,16 @@ const Setup = () => {
 
   return (
     <>
-      <Home />
-      <About />
-      <Partners />
-      <Portfolio />
-      <Contact />
+      {/* <Home /> */}
+      {/* <About /> */}
+      {/* <Partners /> */}
+      {/* <Portfolio /> */}
+      {/* <Contact /> */}
       <PressRoom />
-      <ambientLight color={"#351f4f"} intensity={0.98} />
-      <Lights />
-      <SupportLights />
-      <Plane />
+      {/* <ambientLight color={"#351f4f"} intensity={0.98} /> */}
+      {/* <Lights /> */}
+      {/* <SupportLights /> */}
+      {/* <Plane /> */}
     </>
   );
 };
