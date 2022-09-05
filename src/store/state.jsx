c63535553051c146330fc1dfc76445a1f4a29c6f
Luis Vegas <luisvegasmor@gmail.com>
2022-09-05 09:33:09 -0500

    MapZoom state

--- src/store/state.jsx
+++ src/store/state.jsx
@@ -1,6 +1,6 @@
 import { proxy } from "valtio";
 
-/* Menu state behaves as follows: 
+/* Menu state behaves as follows:
     First scene represents index 1
     Second scene represents index 2
     And so on..*/
@@ -9,4 +9,5 @@ export const state = proxy({
   clicked: 0,
   hovered: null,
   zoom: false,
+  mapZoom: false,
 });
