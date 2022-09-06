41566a1874f20adf84c1aa964418414f98fb37a7
Luis Vegas <luisvegasmor@gmail.com>
2022-09-06 08:52:33 -0500

    feat: envio de datos en el form

--- src/components/scenes/Contact/MonitorWideTexture.jsx
+++ src/components/scenes/Contact/MonitorWideTexture.jsx
@@ -10,24 +10,10 @@ const MonitorWideTexture = (props) => {
   useSnapshot(state);
   const group = useRef();
   const { nodes, materials, animations } = useGLTF(Monitor);
-  const name = document.getElementById("name");
-  const phone = document.getElementById("phone");
-  const email = document.getElementById("email");
-  const message = document.getElementById("message");
-  const correo = {
-    to: "luisvegasmor@gmail.com",
-    from: "noreply@test.com",
-    subject: "Contact SU",
-    text: "Contact SU",
-    html: `<h1>This shit b working fr</h1>`,
-  };
-  const data = new FormData();
-
-  data.append("to", "luisvegasmor@gmail.com");
-  data.append("from", correo.from);
-  data.append("subject", correo.subject);
-  data.append("text", correo.text);
-  data.append("html", correo.html);
+  const name = null;
+  const phone = null;
+  const email = null;
+  const message = null;
 
   const api_key = "874e22a5-cbf8-4da7-bd96-8547289d5343";
   axios.defaults.headers.common = {
@@ -41,7 +27,23 @@ const MonitorWideTexture = (props) => {
         from: "noreply@siteone.com",
         subject: "Testsito",
         text: "Test",
-        html: `<h1> Prueba </h1>`,
+        html: `<h1>Site One received a new message</h1>
+        <br>
+        <ul>
+          <li>
+          name: ${document.getElementById("name").value}
+          </li>
+          <li>
+            Email: ${document.getElementById("email").value}
+          </li>
+          <li>
+            Phone: ${document.getElementById("message").value}
+          </li>
+          <li>
+            Message: ${document.getElementById("message").value}
+          </li>
+        </ul>
+        `,
       })
       .then((res) => {
         console.log("Message was send");
@@ -117,89 +119,93 @@ const MonitorWideTexture = (props) => {
           material={materials["pantalla_wide "]}
           position={[-1.25, 1.9, -0.36]}
         >
-          {/* <> */}
-          {/*   <Html */}
-          {/*     scale={0.3} */}
-          {/*     position={[-3.3, 0.44, -0.98]} */}
-          {/*     style={{ */}
-          {/*       background: "transparent", */}
-          {/*     }} */}
-          {/*     transform */}
-          {/*   > */}
-          {/*     <input */}
-          {/*       id="name" */}
-          {/*       type="text" */}
-          {/*       placeholder="Name" */}
-          {/*       style={{ */}
-          {/*         background: "transparent", */}
-          {/*         border: "none", */}
-          {/*         color: "white", */}
-          {/*       }} */}
-          {/*     /> */}
-          {/*   </Html> */}
-          {/*   <Html */}
-          {/*     scale={0.3} */}
-          {/*     position={[-3.2, -0.18, -0.98]} */}
-          {/*     style={{ */}
-          {/*       background: "transparent", */}
-          {/*     }} */}
-          {/*     transform */}
-          {/*   > */}
-          {/*     <input */}
-          {/*       type="text" */}
-          {/*       id="phone" */}
-          {/*       placeholder="Phone" */}
-          {/*       style={{ */}
-          {/*         background: "transparent", */}
-          {/*         border: "none", */}
-          {/*         color: "white", */}
-          {/*       }} */}
-          {/*     /> */}
-          {/*   </Html> */}
-          {/*   <Html */}
-          {/*     scale={0.3} */}
-          {/*     position={[-3.3, -0.89, -0.98]} */}
-          {/*     rotateX={6} */}
-          {/*     style={{ */}
-          {/*       background: "transparent", */}
-          {/*     }} */}
-          {/*     transform */}
-          {/*   > */}
-          {/*     <input */}
-          {/*       type="text" */}
-          {/*       id="email" */}
-          {/*       placeholder="Email" */}
-          {/*       style={{ */}
-          {/*         background: "transparent", */}
-          {/*         border: "none", */}
-          {/*         color: "white", */}
-          {/*       }} */}
-          {/*     /> */}
-          {/*   </Html> */}
-          {/*   <Html */}
-          {/*     scale={0.3} */}
-          {/*     position={[-0.01, -0.09, -0.98]} */}
-          {/*     rotation={[0, 0.1, 0]} */}
-          {/*     style={{ */}
-          {/*       background: "transparent", */}
-          {/*     }} */}
-          {/*     transform */}
-          {/*   > */}
-          {/*     <textarea */}
-          {/*       rows="10" */}
-          {/*       cols="13" */}
-          {/*       type="text" */}
-          {/*       id="message" */}
-          {/*       placeholder="Message" */}
-          {/*       style={{ */}
-          {/*         background: "transparent", */}
-          {/*         border: "none", */}
-          {/*         color: "white", */}
-          {/*         resize: "none", */}
-          {/*       }} */}
-          {/*     /> */}
-          {/*   </Html> */}
-          {/* </> */}
+          {state.zoom ? (
+            <>
+              <Html
+                scale={0.3}
+                position={[-2.4, 0.44, -0.98]}
+                style={{
+                  background: "transparent",
+                }}
+                transform
+              >
+                <input
+                  id="name"
+                  type="text"
+                  placeholder="Name"
+                  style={{
+                    background: "transparent",
+                    border: "none",
+                    color: "white",
+                  }}
+                />
+              </Html>
+              <Html
+                scale={0.3}
+                position={[-2.4, -0.18, -0.98]}
+                style={{
+                  background: "transparent",
+                }}
+                transform
+              >
+                <input
+                  type="text"
+                  id="phone"
+                  placeholder="Phone"
+                  style={{
+                    background: "transparent",
+                    border: "none",
+                    color: "white",
+                  }}
+                />
+              </Html>
+              <Html
+                scale={0.3}
+                position={[-2.4, -0.89, -0.98]}
+                rotateX={6}
+                style={{
+                  background: "transparent",
+                }}
+                transform
+              >
+                <input
+                  type="text"
+                  id="email"
+                  placeholder="Email"
+                  style={{
+                    background: "transparent",
+                    border: "none",
+                    color: "white",
+                  }}
+                />
+              </Html>
+              <Html
+                scale={0.3}
+                position={[0.7, -0.1, -0.98]}
+                rotation={[0, 0.1, 0]}
+                style={{
+                  background: "transparent",
+                }}
+                transform
+              >
+                <textarea
+                  rows="10"
+                  cols="13"
+                  type="text"
+                  id="message"
+                  placeholder="Message"
+                  style={{
+                    background: "transparent",
+                    border: "none",
+                    color: "white",
+                    resize: "none",
+                  }}
+                />
+              </Html>
+            </>
+          ) : (
+            ""
+          )}
         </mesh>
       </group>
     </group>
